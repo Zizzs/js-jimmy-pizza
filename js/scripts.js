0d8416a0a8fb7d7c6b5461a0dd1a66a632ee44cf
Alex Williams <Zizzs17@gmail.com>
2018-11-02 10:18:56 -0700

    Added ability to create multiple pizzas with unique IDs, unique Toppings, and unique Sizes. Started function to add costs to toppings

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,40 @@
 
 //Pizza Object
 function PizzaObject() {
+    this.pizzaId = 0,
+    this.toppingId = 0,
     this.pizzaSize = [],
-    this.toppings = [],
-    this.currentId = 0
+    this.toppings = []
+}
+PizzaObject.prototype.nextPizzaId = function() {
+    this.pizzaId++;
 }
 
+PizzaObject.prototype.nextPizza = function() {
+    //debugger;
+    if (this.toppings.length > 0) {
+        this.toppings = [];
+        this.toppingId = 0;
+        this.pizzaSize = [];
+    };
+}
 PizzaObject.prototype.addSize = function(size) {
     this.pizzaSize.push(size)
 }
 
 PizzaObject.prototype.addToppings = function(topping) {
-    topping.id = this.assignId();
+    topping.id = this.assignToppingId();
     this.toppings.push(topping);
 }
 
-PizzaObject.prototype.assignId = function() {
-    this.currentId += 1;
-    return this.currentId;
+PizzaObject.prototype.assignToppingId = function() {
+    this.toppingId += 1;
+    return this.toppingId;
+}
+
+PizzaObject.prototype.assignPizzaId = function() {
+    this.pizzaId += 1;
+    return this.pizzaId;
 }
 
 //Size Object
@@ -47,14 +64,16 @@ PizzaObject.prototype.sizeCost = function() {
     } else if (pizzaObject.pizzaSize[0].size === "Extra Large") {
         pizzaObject.pizzaSize[0].cost = 13.99;
     };
+}
 
+PizzaObject.prototype.toppingCost = function() {
+    
 }
 //User Interface Logic
-
 var pizzaObject = new PizzaObject();
-
 $(document).ready(function(event) {
     $("#userInput").submit(function(event) {
+        pizzaObject.nextPizza(); 
         event.preventDefault();
         var cost;
         var pizzaSize = $("#pizzaSizeInput").val();
@@ -76,7 +95,7 @@ $(document).ready(function(event) {
         };
 
         pizzaObject.sizeCost();
-        console.log(pizzaObject); 
-
+        console.log(pizzaObject);
+        pizzaObject.nextPizzaId();
     });
 });
